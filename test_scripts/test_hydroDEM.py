9c3785a56649ee577ac6c93bbd5603be08ef20bc
Theodore Barnhart <tbarnhart@usgs.gov>
2019-06-05 10:56:08 -0600

    added accumulation area comparison

--- test_scripts/test_hydroDEM.py
+++ test_scripts/test_hydroDEM.py
@@ -4,11 +4,11 @@ from make_hydrodem import hydrodem
 
 print("************ Testing HydroDEM ************")
 
-outdir = r"C:\Users\tbarnhart\projects\datapreptools\data\test_workspace\01080207\input_data.gdb"
+outdir = r"C:\Users\tbarnhart\projects\datapreptools\data\test_workspace\01080205\input_data.gdb"
 huc8cov = "huc8"
-origdem = r"C:\Users\tbarnhart\projects\datapreptools\data\test_workspace\01080207\Layers\dem"
+origdem = r"C:\Users\tbarnhart\projects\datapreptools\data\test_workspace\01080205\Layers\dem"
 dendrite = "NHDFlowline"
-snap_grid = r"C:\Users\tbarnhart\projects\datapreptools\data\test_workspace\01080207\Layers\dem"
+snap_grid = r"C:\Users\tbarnhart\projects\datapreptools\data\test_workspace\01080205\Layers\dem"
 bowl_polys = "nhd_wbg"
 bowl_lines = "wb_srcg"
 inwall = "inwall_edit"
@@ -22,7 +22,7 @@ agreesmooth = -500
 agreesharp = -50000
 bowldepth = 2000
 cellsz = 10
-scratchWorkspace = r"C:\Users\tbarnhart\projects\datapreptools\data\test_workspace\01080207\tmp"
+scratchWorkspace = r"C:\Users\tbarnhart\projects\datapreptools\data\test_workspace\01080205\tmp"
 version = None
 
 hydrodem(outdir, huc8cov, origdem, dendrite, snap_grid, bowl_polys, bowl_lines, inwall, drainplug, buffdist, inwallbuffdist, inwallht, outwallht, agreebuf, agreesmooth, agreesharp, bowldepth, cellsz, scratchWorkspace, version = version)
