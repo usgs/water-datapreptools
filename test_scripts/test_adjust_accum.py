be6e9750f3e468620d916a495fd7acd3e95fea41
Theodore Barnhart <tbarnhart@usgs.gov>
2019-06-06 14:33:14 -0600

    tested with multiple uplstream rasters, appears to function correctly; however, bad flow directions are still present.

--- test_scripts/test_adjust_accum.py
+++ test_scripts/test_adjust_accum.py
@@ -6,8 +6,10 @@ print("************ Testing adjust_accum ************")
 
 facPth = "hydrodemfac"
 fdrPth = "hydrodemfdr"
-upstreamFACpths = [r"C:\Users\tbarnhart\projects\datapreptools\data\test_workspace\01080207\input_data.gdb\hydrodemfac"]
-upstreamFDRpths = [r"C:\Users\tbarnhart\projects\datapreptools\data\test_workspace\01080207\input_data.gdb\hydrodemfdr"]
+upstreamFACpths = [r"C:\Users\tbarnhart\projects\datapreptools\data\test_workspace\01080207\input_data.gdb\hydrodemfac",
+					r"C:\Users\tbarnhart\projects\datapreptools\data\comparison\01080206_v4\input_data.gdb\hydrodemfac"]
+upstreamFDRpths = [r"C:\Users\tbarnhart\projects\datapreptools\data\test_workspace\01080207\input_data.gdb\hydrodemfdr",
+					r"C:\Users\tbarnhart\projects\datapreptools\data\comparison\01080206_v4\input_data.gdb\hydrodemfdr"]
 workspace = r"C:\Users\tbarnhart\projects\datapreptools\data\test_workspace\01080205\input_data.gdb"
 #scratch = r"C:\Users\tbarnhart\projects\datapreptools\data\test_workspace\01080205\tmp"
 
