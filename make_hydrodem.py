c338cde993f1ae247b3831153ea936efe874223a
Theodore Barnhart <tbarnhart@usgs.gov>
2019-07-02 12:27:16 -0600

    simplifly constant raster creation

--- make_hydrodem.py
+++ make_hydrodem.py
@@ -442,11 +442,11 @@ def hydrodem(outdir, huc8cov, origdemPth, dendrite, snap_grid, bowl_polys, bowl_
 			arcpy.FeatureToRaster_conversion(drainplug,"dummy",dpg_path,cell_size = cellsz) # (L195 in hydroDEM_work_mod.aml)
 			dpg = Raster(dpg_path) # load the raster object
 		else:
-			tmp = CreateConstantRaster(0, data_type = "INTEGER", cell_size = cellsz) # if the feature class is empty, make a dummy raster
+			tmp = CreateConstantRaster(0) # if the feature class is empty, make a dummy raster
 			dpg = SetNull(tmp,tmp,"VALUE = 0") # set all zeros to null.
 	else:
 		arcpy.AddMessage("	Bypassing Drain Plugs")
-		tmp = CreateConstantRaster(0, data_type = "INTEGER", cell_size = cellsz) # if the feature class is empty, make a dummy raster
+		tmp = CreateConstantRaster(0) # if the feature class is empty, make a dummy raster
 		dpg = SetNull(tmp,tmp,"VALUE = 0") # set all zeros to null.
 
 
