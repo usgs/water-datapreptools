2838161be51e1806ef18666a0ab2c6ee1b30f063
Theodore Barnhart <tbarnhart@usgs.gov>
2019-11-07 11:56:04 -0700

    fixed moon's name

--- test_scripts/test_hydroDEM_mkim.py
+++ test_scripts/test_hydroDEM_mkim.py
@@ -5,11 +5,11 @@ from make_hydrodem import hydrodem
 huc = "0512020116"
 print("************ Testing HydroDEM on unit: %s ************"%huc)
 
-outdir = r"C:\Users\tbarnhart\projects\datapreptools\data\kmoon\%s\input_data.gdb"%(huc)
+outdir = r"C:\Users\tbarnhart\projects\datapreptools\data\mkim\%s\input_data.gdb"%(huc)
 huc8cov = "local"
-origdem = r"C:\Users\tbarnhart\projects\datapreptools\data\kmoon\%s\Layers\dem_raw.tif"%(huc)
+origdem = r"C:\Users\tbarnhart\projects\datapreptools\data\mkim\%s\Layers\dem_raw.tif"%(huc)
 dendrite = "NHDFlowline"
-snap_grid = r"C:\Users\tbarnhart\projects\datapreptools\data\kmoon\%s\Layers\dem_raw.tif"%(huc)
+snap_grid = r"C:\Users\tbarnhart\projects\datapreptools\data\mkim\%s\Layers\dem_raw.tif"%(huc)
 bowl_polys = "nhd_wbg"
 bowl_lines = "wb_srcg"
 inwall = "inwall_edit"
@@ -24,7 +24,7 @@ agreesmooth = -500
 agreesharp = -50000
 bowldepth = 2000
 cellsz = 10
-scratchWorkspace = r"C:\Users\tbarnhart\projects\datapreptools\data\kmoon\%s\tmp"%(huc)
+scratchWorkspace = r"C:\Users\tbarnhart\projects\datapreptools\data\mkim\%s\tmp"%(huc)
 version = None
 
 hydrodem(outdir, huc8cov, origdem, dendrite, snap_grid, bowl_polys, bowl_lines, inwall, drainplug, buffdist, inwallbuffdist, inwallht, outwallht, agreebuf, agreesmooth, agreesharp, bowldepth, cellsz, scratchWorkspace, version = version)
