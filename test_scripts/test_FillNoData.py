0d1e34af2d41be30ac9915e20ac5ec04878d91c0
Theodore Barnhart <tbarnhart@usgs.gov>
2020-05-04 13:24:11 -0600

    removed test scripts

--- test_scripts/test_FillNoData.py
+++
@@ -1,9 +0,0 @@
-from elevationTools import fillNoData
-
-print("************ Testing fillNoData ************")
-
-workspace = InGrid = r"C:\Users\tbarnhart\projects\datapreptools\data\test_workspace\01080207\Layers"
-InGrid = "dem_dd"
-OutGrid = "dem_fill"
-
-fillNoData(workspace, InGrid, OutGrid)
